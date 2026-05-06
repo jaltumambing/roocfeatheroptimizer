723fe157a7636f30d25d4961bf49dc69e644d670
jaltumambing <71112645+jaltumambing@users.noreply.github.com>
2026-05-07 00:59:03 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('*', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`ROOC Feather Optimizer running on port ${PORT}`);
 });
